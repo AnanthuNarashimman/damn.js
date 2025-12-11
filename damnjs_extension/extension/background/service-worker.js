72e16cd13a5e122824af1bce577d8c5b5e3ccc05
AnanthuNarashimman <ananthu.narashimman@gmail.com>
2025-12-11 18:43:38 +0530

    Modified file structure

--- damnjs_extension/extension/background/service-worker.js
+++
@@ -1,6 +0,0 @@
-// Service worker placeholder
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("damn.js extension installed.");
-});
-
-// You will later add message passing & backend calls here.
